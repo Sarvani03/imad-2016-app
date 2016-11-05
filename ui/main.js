da1c9778aded08a88f2138bf84d306b16b322470
Sarvani03 <shruti.chandini@gmail.com>
2016-11-05 17:32:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,13 +5,26 @@ button.onclick=function(){
     
     request.onreadystatechange = function() {
         if(request.readyState==XMLHttpRequest.DONE){
-            if(request.status==200){
+            if(request.status===200){
                  var counter=request.responseText;
                  var span=document.getElementById('count');
                  span.innerHTML=counter.toString();
             }
         }
-    }
-    
-}
+    };
+      reqest.open('GET','http://sarvani03.imad.hasura-aapp.i0'/counter,true);
+      request.send(null);
+};
 
+var nameInput=documet.getElementById('name');
+var name=nameInput.value;
+var submit= document.getElementById('submit_btn');
+submit.onclick =function(){
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+= '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('nameslist');
+    ui.innerHTML=list;
+};
