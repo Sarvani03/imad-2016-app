91c36e6421a690dcd629592cf06ff70079a61b12
Sarvani03 <shruti.chandini@gmail.com>
2016-11-05 16:47:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,17 @@
-console.log('Loaded!');
 
-var element=document.getElementById('main-txt');
-element.innerHTML='New Value';
+var button=document.getElementById('counter');
+button.onclick=function(){
+    var request=new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.status==200){
+                 var counter=request.responseText;
+                 var span=document.getElementById('count');
+                 span.innerHTML=counter.toString();
+            }
+        }
+    }
+    
+}
 
-var imp=document.getElementById('img');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft + 10;
-    img.style.marginLeft=marginLeft+ 'px';
-}
-img.onclick=function() {
-    var interval=setInterval(marginLeft,40);
-   
-}
