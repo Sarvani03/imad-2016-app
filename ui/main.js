584925438379133cca33aba4e9b0d65578d7fc75
Sarvani03 <shruti.chandini@gmail.com>
2016-11-05 16:26:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+var element=document.getElementById('main-txt');
+element.innerHTML='New Value';
+
+var imp=document.getElementById('img');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft + 10;
+    img.style.marginLeft=marginLeft+ 'px';
+}
+img.onclick=function() {
+    var interval=setInterval(marginLeft,40);
+   
+}
