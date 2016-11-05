ad389197c2a42e9dc633adc1f7c42e87ac8195e7
Sarvani03 <shruti.chandini@gmail.com>
2016-11-05 16:26:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,11 @@ var htmpTemplate=
 return htmlTemplate;
 }
     
-
+var counter=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 app.get('/:articleName', function (req, res) {
    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -129,6 +133,9 @@ app.get('/test_db',function(req,res){
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
