07ec0f6035574b347a5ea920d1c5ca04b2f96544
Sarvani03 <shruti.chandini@gmail.com>
2016-11-05 16:47:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,6 +102,8 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
+
+
 app.get('/:articleName', function (req, res) {
    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
