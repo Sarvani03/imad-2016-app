d8e24a0c1574ab737101e425a1a7c56797f06988
Sarvani03 <shruti.chandini@gmail.com>
2016-11-20 18:31:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,15 +30,6 @@ var img=document.getElementById('madi');
 var marginLeft=0;
 var marginRight=0;
 
-function moveRight(){
-    marginLeft=marginLeft-1;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onclick=function(){
-    var interval=setInterval(moveRight,2);
-};
-
 
 
 /*var nameInput=documet.getElementById('name');
