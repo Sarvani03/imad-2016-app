e224b27b874af62e776df3fb28d983f5a839ac50
Sarvani03 <shruti.chandini@gmail.com>
2016-11-21 09:48:02 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ var img=document.getElementById('madi');
  var marginLeft=0;
 
     function moveRight(){
-    marginLeft=marginLeft+10;
+    marginLeft=marginLeft+55;
     img.style.marginLeft=marginLeft+'px';
    
 }
